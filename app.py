ed0329c53d537a1ec390749f3f6b678dc8b882a7
ardha27 <ardhasemaranatha@gmail.com>
2021-11-14 21:35:49 +0800

    status

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
 import sys
-import requests, json, requests.auth
+import requests
+import json
+import requests.auth
 from bs4 import BeautifulSoup
 from flask import Flask, request,  abort
 
@@ -14,13 +17,16 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('Exw6HGDtPSfMXmRUyHnF6dk+CjKjHS81Lg912+69m6/6+7IrxnXzB4OYvjSuO9LZlmMUGwkCq2Wtc78xaCSkd2QjSvhROQBIR+W3fiUUgNkzZ87+v6MjgYq5c643shmrwijeQdtR/iA5lFTJz+KS2gdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    'Exw6HGDtPSfMXmRUyHnF6dk+CjKjHS81Lg912+69m6/6+7IrxnXzB4OYvjSuO9LZlmMUGwkCq2Wtc78xaCSkd2QjSvhROQBIR+W3fiUUgNkzZ87+v6MjgYq5c643shmrwijeQdtR/iA5lFTJz+KS2gdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('45c4bc568a7e1c4cffd08efcec54eee9')
+
 
 @app.route('/', methods=['GET'])
 def handleRoute():
     return "hello"
+
 
 @app.route('/callback', methods=['POST'])
 def callback():
@@ -37,6 +43,7 @@ def callback():
         abort(400)
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     myMessage = event.message.text
@@ -44,12 +51,30 @@ def handle_message(event):
         message = TextSendMessage(text='Yes I am Pain Peko')
         line_bot_api.reply_message(event.reply_token, message)
 
-    elif '/help' in myMessage:
-        message = TextSendMessage(text='1. Currently Live : /live\n2. Search Channel : /channel /member_name\n3. Subscriber Rank : /rank\n4. Live End : /ended\n5. Search Video : /video /video_title\n6. Search Video by Comment : /comment /comment_input\n7. About : /about')
+    if '/help' in myMessage:
+        message = TextSendMessage(
+            text='1. Currently Live : /live\n2. Search Channel : /channel /member_name\n3. Subscriber Rank : /rank\n4. Live End : /ended\n5. Search Video : /video /video_title\n6. Search Video by Comment : /comment /comment_input\n7. About : /about')
         line_bot_api.reply_message(event.reply_token, message)
 
     if '/about' in myMessage:
-        message = TextSendMessage(text='About Creator\n\nWebsite : infoholo.live\nDiscord : discord.gg/jnwPURA\nTrakteer : https://trakteer.id/zirneko\nYoutube : https://www.youtube.com/ZirNeko\nTiktok : https://vt.tiktok.com/ZSJkG5Yxh/\n\nZir Neko 2021\nCreated With HoloAPI (1.0.2)')
+        message = TextSendMessage(
+            text='About Creator\n\nWebsite : infoholo.live\nDiscord : discord.gg/jnwPURA\nTrakteer : https://trakteer.id/zirneko\nYoutube : https://www.youtube.com/ZirNeko\nTiktok : https://vt.tiktok.com/ZSJkG5Yxh/\n\nZir Neko 2021\nCreated With HoloAPI (1.0.2)')
+        line_bot_api.reply_message(event.reply_token, message)
+
+    if '/status' in myMessage:
+        server = mc.lookup(grey-mist.auto.playit.gg)
+        status = server.status()
+        players = ""
+        if status.players.sample is not None:
+            for player in status.players.sample:
+                players += '\n ' + str(player.name)
+        else:
+            players = "No players online"
+        message = ("The Server has {}/{} players online. \n{}".format(
+            status.players.online,
+            status.players.max,
+            str("```" + players + "```")
+        ))
         line_bot_api.reply_message(event.reply_token, message)
 
     command = myMessage.split(' /')
@@ -60,7 +85,8 @@ def handle_message(event):
         message = "Currently Live\n\n"
         cnt = 1
         for live in data['live']:
-            message += str(cnt) + '.' + '\n' + 'Channel : ' + live['channel']['name'] + '\n' + live['title'] + '\n' + 'Currrent View : ' + str(live['live_viewers']) + '\n' + 'Link : https://youtu.be/' + live['yt_video_key'] + '\n' + '\n'
+            message += str(cnt) + '.' + '\n' + 'Channel : ' + live['channel']['name'] + '\n' + live['title'] + '\n' + 'Currrent View : ' + str(
+                live['live_viewers']) + '\n' + 'Link : https://youtu.be/' + live['yt_video_key'] + '\n' + '\n'
             cnt += 1
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
@@ -70,7 +96,10 @@ def handle_message(event):
         message = "Search Channel\n\n"
         cnt = 1
         for channel in data['channels']:
-            message += str(cnt) + '.' + '\n' + 'Channel : ' + channel['name'] + '\n' + 'Subscriber : ' + str(channel['subscriber_count']) + '\n' + 'Link : ' + 'www.youtube.com/channel/' + channel['yt_channel_id'] + '\n' + 'Twitter : '+ 'twitter.com/' + channel['twitter_link'] + '\n\n'
+            message += str(cnt) + '.' + '\n' + 'Channel : ' + channel['name'] + '\n' + 'Subscriber : ' + str(channel['subscriber_count']) + '\n' + \
+                'Link : ' + 'www.youtube.com/channel/' + \
+                channel['yt_channel_id'] + '\n' + 'Twitter : ' + \
+                'twitter.com/' + channel['twitter_link'] + '\n\n'
             cnt += 1
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
@@ -80,7 +109,8 @@ def handle_message(event):
         message = "Subscriber Rank\n\n"
         cnt = 1
         for channel in data['channels']:
-            message += str(cnt) + '.' + '\n' + 'Channel : ' + channel['name'] + '\n' + 'Subscriber : ' + str(channel['subscriber_count']) + '\n' + 'Link : ' + 'www.youtube.com/channel/' + channel['yt_channel_id'] + '\n\n'
+            message += str(cnt) + '.' + '\n' + 'Channel : ' + channel['name'] + '\n' + 'Subscriber : ' + str(
+                channel['subscriber_count']) + '\n' + 'Link : ' + 'www.youtube.com/channel/' + channel['yt_channel_id'] + '\n\n'
             cnt += 1
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
@@ -90,7 +120,9 @@ def handle_message(event):
         message = "Live Ended Within 6 Hours Ago \n\n"
         cnt = 1
         for ended in data['ended']:
-            message += str(cnt) + '.' + '\n' + 'Channel : ' + ended['channel']['name'] + '\n' + ended['title'] + '\n' + 'Link : https://youtu.be/' + ended['yt_video_key'] + '\n' + '\n'
+            message += str(cnt) + '.' + '\n' + 'Channel : ' + ended['channel']['name'] + '\n' + \
+                ended['title'] + '\n' + 'Link : https://youtu.be/' + \
+                ended['yt_video_key'] + '\n' + '\n'
             cnt += 1
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
@@ -100,7 +132,9 @@ def handle_message(event):
         message = "Search Video\n\n"
         cnt = 1
         for video in data['videos']:
-            message += str(cnt) + '.' + '\n' + 'Channel : ' + video['channel']['name'] + '\n' + video['title'] + '\n' + 'Link : https://youtu.be/' + video['yt_video_key'] + '\n\n'
+            message += str(cnt) + '.' + '\n' + 'Channel : ' + video['channel']['name'] + '\n' + \
+                video['title'] + '\n' + 'Link : https://youtu.be/' + \
+                video['yt_video_key'] + '\n\n'
             cnt += 1
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
@@ -110,40 +144,52 @@ def handle_message(event):
         message = "Search Video by Comment\n\n"
         cnt = 1
         for comment in data['comments']:
-            message += str(cnt) + '.' + '\n' + 'Channel : ' + comment['channel']['name'] + '\n' + comment['title'] + '\n' + 'Link : https://youtu.be/' + comment['yt_video_key'] + '\n' + 'Comment : ' + '\n' + comment['comments'][0]['message'] +'\n\n'
+            message += str(cnt) + '.' + '\n' + 'Channel : ' + comment['channel']['name'] + '\n' + comment['title'] + '\n' + \
+                'Link : https://youtu.be/' + \
+                comment['yt_video_key'] + '\n' + 'Comment : ' + \
+                '\n' + comment['comments'][0]['message'] + '\n\n'
             cnt += 1
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
+
 def livesearch():
-    #revieve array 
-    res = requests.get('https://api.holotools.app/v1/live?hide_channel_desc=1&max_upcoming_hours=24')
+    # revieve array
+    res = requests.get(
+        'https://api.holotools.app/v1/live?hide_channel_desc=1&max_upcoming_hours=24')
     return res.content
+
 
 def channelsearch(nama):
     res = requests.get(f'https://api.holotools.app/v1/channels/?name={nama}')
     return res.content
 
+
 def ranksearch():
-    #revieve array 
-    res = requests.get('https://api.holotools.app/v1/channels/?sort=subscriber_count&order=desc&limit=10')
+    # revieve array
+    res = requests.get(
+        'https://api.holotools.app/v1/channels/?sort=subscriber_count&order=desc&limit=10')
     return res.content
 
+
 def endedsearch():
-    #revieve array 
+    # revieve array
     res = requests.get('https://api.holotools.app/v1/live?lookback_hours=6')
     return res.content
 
+
 def videosearch(judul):
-    res = requests.get(f'https://api.holotools.app/v1/videos/?limit=10&title={judul}')
+    res = requests.get(
+        f'https://api.holotools.app/v1/videos/?limit=10&title={judul}')
     return res.content
+
 
 def commentsearch(komen):
-    res = requests.get(f'https://api.holotools.app/v1/comments/search?limit=5&q={komen}')
+    res = requests.get(
+        f'https://api.holotools.app/v1/comments/search?limit=5&q={komen}')
     return res.content
 
-import os
+
 if __name__ == '__main__':
-   
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    
